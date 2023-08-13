b617aa131ffde6764437cb7d141ae959449570c9
qingyue.ke <qingyue.ke@mobvista.com>
2023-08-13 15:51:53 +0800

    commit

--- zs_eyes/__init__.py
+++ zs_eyes/__init__.py
@@ -1,18 +1,22 @@
 import logging
 import os
 
-FORMAT = '%(asctime)s  %(message)s'
-
-logger = logging.getLogger('log')
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler("check.log", 'w', encoding='utf-8')
-handler.setFormatter(logging.Formatter(FORMAT))
-logger.addHandler(handler)
-
-logger.info("set log ....")
 
 host = os.getenv('HOST')
 doctorCode = os.getenv('DOCTOR_CODE')
 deptCode = os.getenv('DEPT_CODE')
 callTel = os.getenv('CALL_TEL')
 pushToken = os.getenv('PUSH_TOKEN')
+logPath = os.getenv('LOG_PATH', 'runtime.log')
+
+
+FORMAT = '%(asctime)s  %(message)s'
+
+logger = logging.getLogger('log')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(logPath, 'w', encoding='utf-8')
+handler.setFormatter(logging.Formatter(FORMAT))
+logger.addHandler(handler)
+
+logger.info("set log ....")
+
